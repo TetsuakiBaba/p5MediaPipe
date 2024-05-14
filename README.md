1f5370141a08ef454dc14f109b26c69d02a4843e
tetsuakibaba <tetsuaki.baba@gmail.com>
2024-05-14 17:33:56 +0900

    update to v0.10.14

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Easy to go mediapipe samples with p5.js!!
 To make it easier to handle mediapipe with p5, I have downloaded the necessary asm and js files for each example. p5MediaPipe branches are created for each version of mediapipe.
 
 ## Version
-* mediapipe@v0.10.13
-* https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.13/
+* mediapipe@v0.10.14
+* https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.14/
 
 ## Examples
 * hands-landmarker : <a href="https://tetsuakibaba.github.io/p5MediaPipe/hands-landmarker/" target="_blank">DEMO</a>
@@ -22,6 +22,12 @@ target="_blank">DEMO</a>
   * Only this example uses vision_bundle.js of v0.10.2 because vision_bundle.js of v0.10.13 causes an error.
 * face landmark detection: <a href="https://tetsuakibaba.github.io/p5MediaPipe/face-landmarker/" target="_blank">DEMO</a>
 * pose landmark detection: <a href="https://tetsuakibaba.github.io/p5MediaPipe/pose-landmarker/" target="_blank">DEMO</a>
+
+## How to update mediapipe version
+```
+npm update @mediapipe/tasks-vision
+sh copy_files.sh
+```
 
 
 ## Reference
---
+++ copy_files.sh
@@ -0,0 +1,21 @@
+SOURCE_DIR="node_modules/@mediapipe/tasks-vision"
+# vision_bundle.mjs を face-landmarker/js/ に vision_bundle.js としてコピー
+cp $SOURCE_DIR/vision_bundle.mjs face-landmarker/js/vision_bundle.js
+cp $SOURCE_DIR/vision_bundle.mjs hands-landmarker/js/vision_bundle.js
+cp $SOURCE_DIR/vision_bundle.mjs hands-landmarker-gesture-recognition/js/vision_bundle.js
+cp $SOURCE_DIR/vision_bundle.mjs image-classification/js/vision_bundle.js
+#cp $SOURCE_DIR/vision_bundle.mjs image-segmentation/js/vision_bundle.js
+cp $SOURCE_DIR/vision_bundle.mjs object-detection/js/vision_bundle.js
+cp $SOURCE_DIR/vision_bundle.mjs object-detection-mobile-web-app-template/js/vision_bundle.js
+cp $SOURCE_DIR/vision_bundle.mjs pose-landmarker/js/vision_bundle.js
+
+# SOURCE_DIR/wasm ディレクトリを face-landmarker/ 以下にコピー（上書き）
+cp -r $SOURCE_DIR/wasm face-landmarker/
+cp -r $SOURCE_DIR/wasm hands-landmarker/
+cp -r $SOURCE_DIR/wasm hands-landmarker-gesture-recognition/
+cp -r $SOURCE_DIR/wasm pose-landmarker/
+cp -r $SOURCE_DIR/wasm image-classification/
+#cp -r $SOURCE_DIR/wasm image-segmentation/
+cp -r $SOURCE_DIR/wasm object-detection/
+cp -r $SOURCE_DIR/wasm object-detection-mobile-web-app-template/
+
